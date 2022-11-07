12ff6db81720e4f25ff158199ebf61359fd54d71
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2022-11-07 11:35:53 +0530

    commit

---
+++ src/basicPrograms/TernaryOperator.java
@@ -0,0 +1,10 @@
+package basicPrograms;
+
+public class TernaryOperator{
+	public static void main(String[] args){
+		int a = 2;
+		int b = 5;
+		int min = (a < b) ? a:b;
+		System.out.println(min);`
+}
+}
