c46dd395435b294f4f2a8ded896d759dd64b093d
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2022-11-07 11:53:46 +0530

    commit

--- src/basicPrograms/IfExample.java
+++ src/basicPrograms/IfExample.java
@@ -3,16 +3,15 @@ package basicPrograms;
 import java.util.Scanner;
 
 public class IfExample {
-	public static void main(String[] args){
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("Enter your age : ");
-	int age = scanner.nextInt();
-	scanner.close();
-	if(age >= 18){
-	System.out.println("You are allowed to enter!!");	
-} else {
-	System.out.println("You are not allowed to enter!!");
-}
-
-}
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age : ");
+        int age = scanner.nextInt();
+        scanner.close();
+        if(age >= 18){
+            System.out.println("You are allowed to Enter!!");
+        } else {
+            System.out.println("You are not allowed to Enter!!");
+        }
+    }
 }
