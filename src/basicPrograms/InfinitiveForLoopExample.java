638f4f718cb8f36b9d4a6991d9eade1a73ee9dd9
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2022-11-17 14:26:05 +0530

    commit

---
+++ src/basicPrograms/InfinitiveForLoopExample.java
@@ -0,0 +1,9 @@
+package basicPrograms;
+
+public class InfinitiveForLoopExample{
+    public static void main(String[] args){
+        for(;;){
+            System.out.println("Infinitive...");
+        }
+    }
+}
